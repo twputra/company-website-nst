57fa9c5af4f7975bb08a9a40656122ef947702e1
Try Widodo Putra <trywidodoputra@gmail.com>
2023-06-12 15:00:16 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -11,31 +11,23 @@ import {
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Features from "./components/Features";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+<>
+    <Navbar />
+    <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Features />
+      <Footer />
+    
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,15 +4,22 @@ import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
+  <ChakraProvider>
+    <BrowserRouter>
   <StrictMode>
     <ColorModeScript />
     <App />
   </StrictMode>
+  </BrowserRouter>
+  </ChakraProvider>
 );
 
 // If you want your app to work offline and load faster, you can change
